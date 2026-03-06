8200dc21338c72327ddb37b416f60ccfd72c0c90
Julian <chaparrobarrerajulian@gmail.com>
2026-03-06 16:56:01 -0500

    ok

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8000/api';
+//export const API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = 'https://backend-manejohorarioscimm.sgdis.cloud/api';
 import { getToken, logout } from './auth.js';
 
 /**
