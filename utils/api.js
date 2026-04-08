6705245e6cf4388623b1f8d319b7a0ebe03a12e9
Julian <chaparrobarrerajulian@gmail.com>
2026-04-08 13:47:54 -0500

    ok fix bugs maybe

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8000/api';
-//export const API_BASE_URL = 'http://173.249.44.159:8000/api';
+//export const API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = 'http://173.249.44.159:8000/api';
 
 import { getToken, logout } from './auth.js';
 
